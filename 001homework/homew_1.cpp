92098476b93dbe3cc307f9d6873b470e40862e80
SvetlanaErmakova <Svetlana.lit1533@gmail.com>
2021-09-10 01:22:49 +0300

    /

---
+++ 001homework/homew_1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include "CIntN.hpp"
+#include "Autotest.hpp"
+using namespace std;
+
+int main()
+{
+    if (Autotest() != 1)
+    {
+        cout << "Error! Tests not passed!\n";
+    }
+    else
+    {
+        CIntN V2, V3;
+        try
+        {
+            CIntN V = CIntN(33);
+
+            cout << "rez op+ is: ";
+            V3 = V + V2;
+            cout << V3.Value()<<'\n';
+
+            cout << "rez op- is: ";
+            V3 = V - V2;
+            cout << V3.Value()<<'\n';
+        }
+        catch (...) { cout << "\nincorrect for this class" << endl; }
+
+        return 0;
+    }
+}
+
